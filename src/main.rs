c76072b574e948cf516c78069723a4c49305194f
David Cosby <davjcosby@gmail.com>
2024-03-29 23:26:27 -0600

    added comet

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use rs_ws281x::{ChannelBuilder, Controller, ControllerBuilder};
 use sled::{color::Srgb, Sled};
 
-mod ripples;
+mod comet;
 
 fn main() {
     let sled = Sled::new("./config.toml").unwrap();
     let num_leds = sled.num_leds();
-    let mut driver = ripples::build_driver();
+    let mut driver = comet::build_driver();
     driver.mount(sled);
 
     let mut gpio_controller = construct_gpio_controller(num_leds);
